6c781709eaa43d717ef0adc999a0e7073eca91b2
Shchukin Ivan <shchukin@arrival.com>
2021-10-10 00:10:20 +0300

    Add generating protobuf files

--- client/README.md
+++ client/README.md
@@ -2,15 +2,15 @@
 
 A new Flutter project.
 
-## Getting Started
+## Presequinces
 
-This project is a starting point for a Flutter application.
+* Flutter stable 2.2.3
 
-A few resources to get you started if this is your first Flutter project:
-
-- [Lab: Write your first Flutter app](https://flutter.dev/docs/get-started/codelab)
-- [Cookbook: Useful Flutter samples](https://flutter.dev/docs/cookbook)
-
-For help getting started with Flutter, view our
-[online documentation](https://flutter.dev/docs), which offers tutorials,
-samples, guidance on mobile development, and a full API reference.
+1. Install required dependencies with
+```sh
+make install
+```
+2. Update your `PATH` variable in file like `~/.bash_profile` with
+```sh
+export PATH="$PATH:$HOME/.pub-cache/bin"
+```
---
+++ client/lib/gen/spot/v1/spot_v1.pbjson.dart
@@ -0,0 +1,27 @@
+///
+//  Generated code. Do not modify.
+//  source: spot/v1/spot_v1.proto
+//
+// @dart = 2.12
+// ignore_for_file: annotate_overrides,camel_case_types,unnecessary_const,non_constant_identifier_names,library_prefixes,unused_import,unused_shown_name,return_of_invalid_type,unnecessary_this,prefer_final_fields,deprecated_member_use_from_same_package
+
+import 'dart:core' as $core;
+import 'dart:convert' as $convert;
+import 'dart:typed_data' as $typed_data;
+@$core.Deprecated('Use createSpotRequestDescriptor instead')
+const CreateSpotRequest$json = const {
+  '1': 'CreateSpotRequest',
+};
+
+/// Descriptor for `CreateSpotRequest`. Decode as a `google.protobuf.DescriptorProto`.
+final $typed_data.Uint8List createSpotRequestDescriptor = $convert.base64Decode('ChFDcmVhdGVTcG90UmVxdWVzdA==');
+@$core.Deprecated('Use createSpotResponseDescriptor instead')
+const CreateSpotResponse$json = const {
+  '1': 'CreateSpotResponse',
+  '2': const [
+    const {'1': 'uuid', '3': 1, '4': 1, '5': 9, '10': 'uuid'},
+  ],
+};
+
+/// Descriptor for `CreateSpotResponse`. Decode as a `google.protobuf.DescriptorProto`.
+final $typed_data.Uint8List createSpotResponseDescriptor = $convert.base64Decode('ChJDcmVhdGVTcG90UmVzcG9uc2USEgoEdXVpZBgBIAEoCVIEdXVpZA==');
