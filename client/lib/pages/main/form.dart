2330ac21d7b8ac43297a59759aa054481112155d
Shchukin Ivan <shchukin@arrival.com>
2021-10-09 19:32:56 +0300

    Move title to page

--- client/lib/pages/main/form.dart
+++ client/lib/pages/main/form.dart
@@ -5,7 +5,6 @@ import 'package:flutter_bloc/flutter_bloc.dart';
 // Internal
 import 'package:dots_client/pages/main/bloc/events.dart';
 import 'package:dots_client/pages/main/bloc/bloc.dart';
-import 'package:dots_client/theme.dart';
 import 'package:dots_client/pages/main/bloc/state.dart';
 
 class MainForm extends StatelessWidget {
@@ -13,41 +12,25 @@ class MainForm extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(title: const Text('Dots')),
-      body: BlocBuilder<MainPageBloc, MainPageState>(
-        builder: (context, state) {
-          if (state is InitedState) {
-            return Center(
-              child: ElevatedButton(
-                key: const Key("btn_create_spot"),
-                child: const Text("Create new spot"),
-                onPressed: () =>
-                    context.read<MainPageBloc>().add(CreateNewSpotEvent()),
-              ),
-            );
-          } else if (state is CreatingNewSpotState) {
-            return const Center(
-              child: CircularProgressIndicator(),
-            );
-          }
-
-          return Text("Unkown state: $state");
-        },
-      ),
-      floatingActionButton: Column(
-        crossAxisAlignment: CrossAxisAlignment.end,
-        mainAxisAlignment: MainAxisAlignment.end,
-        children: <Widget>[
-          Padding(
-            padding: const EdgeInsets.symmetric(vertical: 5.0),
-            child: FloatingActionButton(
-              child: const Icon(Icons.brightness_6),
-              onPressed: () => context.read<ThemeCubit>().toggleTheme(),
+    return BlocBuilder<MainPageBloc, MainPageState>(
+      builder: (context, state) {
+        if (state is InitedState) {
+          return Center(
+            child: ElevatedButton(
+              key: const Key("btn_create_spot"),
+              child: const Text("Create new spot"),
+              onPressed: () =>
+                  context.read<MainPageBloc>().add(CreateNewSpotEvent()),
             ),
-          ),
-        ],
-      ),
+          );
+        } else if (state is CreatingNewSpotState) {
+          return const Center(
+            child: CircularProgressIndicator(),
+          );
+        }
+
+        return Text("Unkown state: $state");
+      },
     );
   }
 }
--- client/lib/pages/main/page.dart
+++ client/lib/pages/main/page.dart
@@ -1,9 +1,10 @@
 // External
-import 'package:dots_client/pages/main/form.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 
 // Internal
+import 'package:dots_client/pages/main/form.dart';
+import 'package:dots_client/theme.dart';
 import 'package:dots_client/pages/main/bloc/bloc.dart';
 
 class MainPage extends StatelessWidget {
@@ -13,7 +14,22 @@ class MainPage extends StatelessWidget {
   Widget build(BuildContext context) {
     return BlocProvider(
       create: (_) => MainPageBloc(),
-      child: const MainForm(),
+      child: Scaffold(
+          appBar: AppBar(title: const Text('Dots')),
+          body: const MainForm(),
+          floatingActionButton: Column(
+            crossAxisAlignment: CrossAxisAlignment.end,
+            mainAxisAlignment: MainAxisAlignment.end,
+            children: <Widget>[
+              Padding(
+                padding: const EdgeInsets.symmetric(vertical: 5.0),
+                child: FloatingActionButton(
+                  child: const Icon(Icons.brightness_6),
+                  onPressed: () => context.read<ThemeCubit>().toggleTheme(),
+                ),
+              ),
+            ],
+          )),
     );
   }
 }
