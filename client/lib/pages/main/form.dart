d112d21b9d90c3ace83556062648d690f8406d02
Shchukin Ivan <shchukin@arrival.com>
2021-10-10 13:08:42 +0300

    Add map

--- client/lib/pages/main/form.dart
+++ client/lib/pages/main/form.dart
@@ -1,6 +1,8 @@
 // External
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:flutter_map/plugin_api.dart';
+import 'package:latlong2/latlong.dart';
 
 // Internal
 import 'bloc/events.dart';
@@ -16,13 +18,44 @@ class MainForm extends StatelessWidget {
       builder: (context, state) {
         if (state is InitedState) {
           return Center(
-            child: ElevatedButton(
-              key: const Key("btn_create_spot"),
-              child: const Text("Create new spot"),
-              onPressed: () =>
-                  context.read<MainPageBloc>().add(CreateNewSpotEvent()),
-            ),
-          );
+              child: Stack(
+            children: [
+              FlutterMap(
+                options: MapOptions(
+                  center: LatLng(51.5, -0.09),
+                  zoom: 13.0,
+                ),
+                layers: [
+                  TileLayerOptions(
+                    urlTemplate:
+                        "https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png",
+                    subdomains: ['a', 'b', 'c'],
+                    attributionBuilder: (_) {
+                      return Text("© OpenStreetMap contributors");
+                    },
+                  ),
+                  MarkerLayerOptions(
+                    markers: [
+                      Marker(
+                        width: 80.0,
+                        height: 80.0,
+                        point: LatLng(51.5, -0.09),
+                        builder: (ctx) => Container(
+                          child: FlutterLogo(),
+                        ),
+                      ),
+                    ],
+                  ),
+                ],
+              ),
+              ElevatedButton(
+                key: const Key("btn_create_spot"),
+                child: const Text("Create new spot"),
+                onPressed: () =>
+                    context.read<MainPageBloc>().add(CreateNewSpotEvent()),
+              ),
+            ],
+          ));
         } else if (state is CreatingNewSpotState) {
           return const Center(
             child: CircularProgressIndicator(),
