7a26a62832f0a08062412202e1cc558c220fc118
Shchukin Ivan <shchukin@arrival.com>
2021-10-13 16:30:40 +0300

    Optimise UI

--- client/lib/pages/spot_settings/form.dart
+++ client/lib/pages/spot_settings/form.dart
@@ -24,65 +24,12 @@ class SpotSettingsForm extends StatelessWidget {
       builder: (context, state) {
         final curState = state;
         if (curState is InitedState) {
-          return Center(
-            child: SingleChildScrollView(
-              child: Column(
-                mainAxisAlignment: MainAxisAlignment.center,
-                children: [
-                  const Text("Location"),
-                  SizedBox(
-                    width: 300,
-                    height: 300,
-                    child: MapWidget(
-                      mapController: mapController,
-                      position: curState.position,
-                      zoom: 17.0,
-                    ),
-                  ),
-                  const Text("Radius in meters"),
-                  NumberPicker(
-                    axis: Axis.horizontal,
-                    step: 10,
-                    minValue: 0,
-                    maxValue: 1000,
-                    value: curState.radius,
-                    onChanged: (value) => context
-                        .read<SpotSettingsPageBloc>()
-                        .add(NewRadiusEvent(value: value)),
-                  ),
-                  const Text("Scan time in seconds"),
-                  NumberPicker(
-                    axis: Axis.horizontal,
-                    step: 10,
-                    minValue: 0,
-                    maxValue: 1000,
-                    value: curState.scanPeriod.inSeconds,
-                    onChanged: (value) => context
-                        .read<SpotSettingsPageBloc>()
-                        .add(NewScanDurationEvent(
-                            value: Duration(seconds: value))),
-                  ),
-                  const Text("Zone time in seconds"),
-                  NumberPicker(
-                    axis: Axis.horizontal,
-                    step: 10,
-                    minValue: 0,
-                    maxValue: 1000,
-                    value: curState.zonePeriod.inSeconds,
-                    onChanged: (value) => context
-                        .read<SpotSettingsPageBloc>()
-                        .add(NewZoneDurationEvent(
-                            value: Duration(seconds: value))),
-                  ),
-                  _CreateNewSpotBtn(
-                    position: curState.position,
-                    radius: curState.radius,
-                    scanPeriod: curState.scanPeriod,
-                    zonePeriod: curState.zonePeriod,
-                  ),
-                ],
-              ),
-            ),
+          return _SpotSettingsForm(
+            mapController: mapController,
+            position: curState.position,
+            radius: curState.radius,
+            scanPeriod: curState.scanPeriod,
+            zonePeriod: curState.zonePeriod,
           );
         } else if (curState is CreatingNewSpotState) {
           // TODO Implement
@@ -101,6 +48,86 @@ class SpotSettingsForm extends StatelessWidget {
 
         return Text("Unkown state: $state");
       },
+    );
+  }
+}
+
+class _SpotSettingsForm extends StatelessWidget {
+  final MapController mapController;
+  final LatLng position;
+  // Spot radius in meters
+  final int radius;
+  final Duration scanPeriod;
+  final Duration zonePeriod;
+
+  const _SpotSettingsForm({
+    required this.position,
+    required this.mapController,
+    required this.radius,
+    required this.scanPeriod,
+    required this.zonePeriod,
+    Key? key,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Center(
+      child: SingleChildScrollView(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            const Text("Location"),
+            SizedBox(
+              width: 300,
+              height: 300,
+              child: MapWidget(
+                mapController: mapController,
+                position: position,
+                zoom: 17.0,
+              ),
+            ),
+            const Text("Radius in meters"),
+            NumberPicker(
+              axis: Axis.horizontal,
+              step: 10,
+              minValue: 0,
+              maxValue: 1000,
+              value: radius,
+              onChanged: (value) => context
+                  .read<SpotSettingsPageBloc>()
+                  .add(NewRadiusEvent(value: value)),
+            ),
+            const Text("Scan time in seconds"),
+            NumberPicker(
+              axis: Axis.horizontal,
+              step: 10,
+              minValue: 0,
+              maxValue: 1000,
+              value: scanPeriod.inSeconds,
+              onChanged: (value) => context
+                  .read<SpotSettingsPageBloc>()
+                  .add(NewScanDurationEvent(value: Duration(seconds: value))),
+            ),
+            const Text("Zone time in seconds"),
+            NumberPicker(
+              axis: Axis.horizontal,
+              step: 10,
+              minValue: 0,
+              maxValue: 1000,
+              value: zonePeriod.inSeconds,
+              onChanged: (value) => context
+                  .read<SpotSettingsPageBloc>()
+                  .add(NewZoneDurationEvent(value: Duration(seconds: value))),
+            ),
+            _CreateNewSpotBtn(
+              position: position,
+              radius: radius,
+              scanPeriod: scanPeriod,
+              zonePeriod: zonePeriod,
+            ),
+          ],
+        ),
+      ),
     );
   }
 }
