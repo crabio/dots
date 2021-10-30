920f8196418de02a65025c3bd10364132b67ed63
iakrevetkho <shchukin.ivan.v@gmail.com>
2021-10-30 23:38:42 +0300

    Fix client tests

--- client/test/widget_test.dart
+++ client/test/widget_test.dart
@@ -1,17 +1,23 @@
-// External
 import 'package:dots_client/pages/main/page.dart';
 import 'package:dots_client/pages/settings/page.dart';
+import 'package:dots_client/settings/controller.dart';
 import 'package:dots_client/settings/settings.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
-
-// Internal
+import 'package:mockito/annotations.dart';
+import 'package:mockito/mockito.dart';
 import 'package:dots_client/main.dart';
+import 'widget_test.mocks.dart';
 
+@GenerateMocks([AppSettingsController])
 void main() {
   testWidgets('Go to settings', (WidgetTester tester) async {
+    final appSettingsController = MockAppSettingsController();
+    // Stub a mock methods before interacting.
+    when(appSettingsController.settings).thenReturn(AppSettings());
+
     // Build app
-    await tester.pumpWidget(App(settings: AppSettings()));
+    await tester.pumpWidget(App(appSettingsController: appSettingsController));
 
     // Check that app on main page
     expect(find.byType(MainPage), findsOneWidget);
