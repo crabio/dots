8ac1a3b561f19539f3252ebd6f30baf86d32b6c3
Shchukin Ivan <shchukin@arrival.com>
2021-11-14 11:23:36 +0000

    Stop zone controller on game end

--- server/pkg/spot_session/session.go
+++ server/pkg/spot_session/session.go
@@ -6,19 +6,23 @@ import (
 
 	"github.com/golang/geo/s2"
 	"github.com/google/uuid"
+	"github.com/iakrevetkho/archaeopteryx/logger"
 	"github.com/iakrevetkho/dots/server/pkg/damage"
 	"github.com/iakrevetkho/dots/server/pkg/game_controller"
 	"github.com/iakrevetkho/dots/server/pkg/player_state"
 	"github.com/iakrevetkho/dots/server/pkg/zone"
+	"github.com/sirupsen/logrus"
 	"github.com/tjgq/broadcast"
 )
 
 type SpotSession struct {
 	sync.RWMutex
 
+	log *logrus.Entry
+
 	ZoneController *zone.Controller
 
-	Controller *damage.Controller
+	DamageController *damage.Controller
 
 	GameController *game_controller.GameController
 	// Channel for sending game event (one of StartGameEvent, EndGameEvent)
@@ -34,6 +38,7 @@ type SpotSession struct {
 
 func NewSpotSession(spotPosition s2.LatLng, spotRadiusInM float32, nextZonePeriod time.Duration, duration time.Duration) *SpotSession {
 	ss := new(SpotSession)
+	ss.log = logger.CreateLogger("zone-controller-" + uuid.NewString())
 	ss.ZoneController = zone.NewController(spotPosition, spotRadiusInM, 10, nextZonePeriod, 15*time.Second, 20.0)
 	ss.GameController = game_controller.NewGameController(duration)
 	ss.GameEventBroadcaster = broadcast.New(0)
@@ -51,7 +56,7 @@ func (ss *SpotSession) Start(hunterUuid uuid.UUID, playersList []uuid.UUID) erro
 		ss.PlayersStateMap.Store(playerUuid, playerState)
 	}
 
-	ss.Controller = damage.NewDamageController(ss.ZoneController.ZoneEventBroadcaster, ss.PlayersStateMap)
+	ss.DamageController = damage.NewDamageController(ss.ZoneController.ZoneEventBroadcaster, ss.PlayersStateMap)
 
 	// Start game
 	ss.GameController.Start(hunterUuid)
@@ -74,7 +79,7 @@ func (ss *SpotSession) NewPlayersState(key uuid.UUID, value *player_state.Player
 	ss.PlayersStateMap.Store(key, value)
 
 	// Send new player position to damage controller
-	ss.Controller.NewPlayerState(key, value)
+	ss.DamageController.NewPlayerState(key, value)
 
 	event, err := ss.GameController.Check(ss.PlayersStateMap)
 	if err != nil {
@@ -84,12 +89,16 @@ func (ss *SpotSession) NewPlayersState(key uuid.UUID, value *player_state.Player
 	// Check that we have event
 	if event != nil {
 		ss.sendEndGameEvent(event)
+
+		// Stop zone controller
+		ss.ZoneController.Stop()
 	}
 
 	return nil
 }
 
 func (ss *SpotSession) sendStartGameEvent() {
+	ss.log.Debug("sendStartGameEvent")
 	event := game_controller.StartGameEvent{}
 	ss.LastGameEvent = event
 
@@ -98,6 +107,7 @@ func (ss *SpotSession) sendStartGameEvent() {
 }
 
 func (ss *SpotSession) sendEndGameEvent(event *game_controller.EndGameEvent) {
+	ss.log.Debug("sendEndGameEvent")
 	ss.LastGameEvent = *event
 
 	// Broadcast event
